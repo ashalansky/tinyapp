dd0ffe4b05bfe0905a86bd61de13e6e7c2ed6e15
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-05 21:18:17 +0000

    display shorturl and cooresponding long url

--- express_server.js
+++ express_server.js
@@ -12,8 +12,14 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.shortURL};
+  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {
