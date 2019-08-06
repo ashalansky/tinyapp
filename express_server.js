4d8a16df8235581768d9154e8a95a9c269bf0a59
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-06 01:40:05 +0000

    implemented generate random string

--- express_server.js
+++ express_server.js
@@ -5,12 +5,20 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8);
+}
+console.log(generateRandomString());
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
