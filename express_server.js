cec8401777bc98dafef31c00d2ed27d8814c68a6
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-06 20:27:33 +0000

    adding comments to make clear what each POST/GET is doing

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 
 let urlDatabase = {
@@ -14,15 +14,14 @@ let urlDatabase = {
 function generateRandomString() {
   return Math.random().toString(36).slice(2, 8);
 }
-// let shortURL = (generateRandomString());
-// urlDatabase[shortURL] = {shortURL: shortURL, longURL: urlDatabase[shortURL]};
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   console.log(req.body.longURL);
-  // console.log(urlDatabase);  // Log the POST request body to the console
-  res.redirect(`/urls/${shortURL}`); //responds with a redirect to /urls/:shortURL 
+  // console.log(urlDatabase);
+  // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`);//responds with a redirect to /urls/:shortURL 
 });
 
 app.get("/urls/new", (req, res) => {
@@ -32,12 +31,12 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
+});// pass URL data to our template
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]};//Use the shortURL from the route parameter to lookup it's associated longURL from the urlDatabase
-  res.render("urls_show", templateVars);
+  res.render("urls_show", templateVars); //render information about a single URL
 });
 
 app.get("/", (req, res) => {
