1804da6fafa1209fa969ee07220e95e865424754
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-06 15:42:53 +0000

    added random generator to display long and short url in database

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -14,20 +14,21 @@ const urlDatabase = {
 function generateRandomString() {
   return Math.random().toString(36).slice(2, 8);
 }
-console.log(generateRandomString());
+let shortURL = (generateRandomString());
+urlDatabase[shortURL] = {shortURL: shortURL, longURL: urlDatabase[shortURL]};
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);  //responds with a redirect to /urls/:shortURL
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
