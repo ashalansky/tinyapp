6b2ecc24cc6f30a4ec92f7f28b01bf2654ff4fd8
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-06 20:42:42 +0000

    added comments

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true})); // convert request body from a Buffer into string
 app.set("view engine", "ejs");
 
 
@@ -25,7 +25,7 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new"); // present form to user
 });
 
 app.get("/urls", (req, res) => {
