340ed9d403028c88245d03debbfd014207b39962
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-07 00:44:11 +0000

    added comments and separation marks. Redirected short URL's to long URL website

--- express_server.js
+++ express_server.js
@@ -15,17 +15,24 @@ function generateRandomString() {
   return Math.random().toString(36).slice(2, 8);
 }
 
+//CREATE NEW
+// integrate long url inside of short url
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
+  let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  console.log(req.body.longURL);
-  // console.log(urlDatabase);
-  // Log the POST request body to the console
+  console.log(req.body);
   res.redirect(`/urls/${shortURL}`);//responds with a redirect to /urls/:shortURL 
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new"); // present form to user
+});
+
+// SHORT URLS INTO LINKS
+// redirent to long url website
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls", (req, res) => {
@@ -34,13 +41,20 @@ app.get("/urls", (req, res) => {
 });// pass URL data to our template
 
 app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]};//Use the shortURL from the route parameter to lookup it's associated longURL from the urlDatabase
+  let shortURL = req.params.shortURL; 
+  let templateVars = { shortURL: shortURL, longURL: urlDatabase[req.params.shortURL]};//Use the shortURL from the route parameter to lookup it's associated longURL from the urlDatabase
   res.render("urls_show", templateVars); //render information about a single URL
 });
 
+//DELETE
+//EDIT
+//LOGIN
+//LOGOUT
+//REGISTRATION
+//NEW USER REG WILL ENCRYPT PASSWORD
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
